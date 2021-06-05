58a7ee6ca59253d751b4dfcaaa6364562057ddf3
Txp <1548255353@qq.com>
2021-06-05 17:29:31 +0800

    commit

---
+++ springboot-021-dubbo-provider/src/main/java/com/bjpowernode/springboot/impl/StudentServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bjpowernode.springboot.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.bjpowernode.springboot.service.StudentService;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Service(interfaceClass = StudentService.class,version = "1.0.0",timeout = 15000)
+public class StudentServiceImpl implements StudentService {
+    @Override
+    public Integer queryAllStudentCount() {
+        return 1200;
+    }
+}
